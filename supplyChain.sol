dc5908ca7c5a348eea79c1f47da1d76e3a514c79
Soroosh Jaberi <sorooshjaberi@gmail.com>
2022-07-26 01:09:29 +0430

    address of deliver persion

--- supplyChain.sol
+++ supplyChain.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 contract dealer{
 
     enum productState {created,paid,delivered}
@@ -14,8 +14,11 @@ contract dealer{
         productState state;
         string productName ;
         uint priceInWei;
+        address buyer;
     }
-
+    function add() view  public returns(address){
+        return msg.sender;
+    }
     function createProduct(string memory _name, uint _priceInWei) public {
         items[index].productName = _name;
         items[index].priceInWei = _priceInWei;
@@ -26,12 +29,14 @@ contract dealer{
     function payProduct(uint _index) payable public{
         require(items[_index].priceInWei <= msg.value, "Not fully paid");
         require(items[_index].state==productState.created, "we don't have it yet");
-        items[index].state = productState.paid;
+        items[_index].state = productState.paid;
+        items[_index].buyer = msg.sender;
         emit step(_index,uint(items[_index].state));
     }
 
     function deliveredProduct(uint _index) public{
         require(items[_index].state==productState.created, "we don't have it yet");
+        require(items[_index].buyer==msg.sender, "you didn't bought this");
         items[index].state = productState.delivered;
         emit step(_index,uint(items[_index].state));
 
