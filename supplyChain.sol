328f9ea1ab29f08857e6e240fc9ce183943dc4df
Soroosh Jaberi <sorooshjaberi@gmail.com>
2022-07-28 15:35:54 +0430

    ownable + item contract

--- supplyChain.sol
+++ supplyChain.sol
@@ -1,29 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-contract dealer{
+
+contract Ownable {
+    address owner;
+
+    constructor(){
+        owner=msg.sender;
+    }
+    function isOwner() public view returns(bool){
+        return msg.sender ==owner;
+    }
+    modifier onlyOwner(){
+        require(isOwner(),"onlyOwner");
+        _;
+    }
+
+
+}
+
+contract item{
+
+    uint priceInWei;
+    uint pricePaid;
+    uint index;
+    itemManager parentContract;
+
+    constructor(uint _priceInWei, uint _index, itemManager _parentContract){
+    priceInWei=_priceInWei;
+    index=_index;
+    parentContract=_parentContract;
+    }
+    receive() external payable{
+        require(priceInWei==msg.value,"pay completeley");
+        require(pricePaid == 0,"item is already paid");
+        pricePaid+=msg.value;
+       (bool status,)=address(parentContract).call{value:msg.value}(abi.encodeWithSignature("payProduct(uint)",index));
+       require(status,"something went wrong, canceling the transaction");
+    }
+    fallback()external{}
+}
+
+
+
+contract itemManager is Ownable{
 
     enum productState {created,paid,delivered}
 
     mapping (uint => Product) public items;
 
-    event step(uint _itemIndex, uint state);
+    event step(uint _itemIndex, uint state, address itemContract);
 
     uint index;
     struct Product {
+        
         productState state;
         string productName ;
         uint priceInWei;
         address buyer;
+        item item;
     }
     function add() view  public returns(address){
         return msg.sender;
     }
-    function createProduct(string memory _name, uint _priceInWei) public {
+    function createProduct(string memory _name, uint _priceInWei) public onlyOwner{
+        item ItemContract = new item(_priceInWei, index,this);
         items[index].productName = _name;
         items[index].priceInWei = _priceInWei;
         items[index].state = productState.created;
-        emit step(index,uint(items[index].state));
+        items[index].item= ItemContract;
+        emit step(index,uint(items[index].state), address(items[index].item));
         index++;
     }
     function payProduct(uint _index) payable public{
@@ -31,14 +77,14 @@ contract dealer{
         require(items[_index].state==productState.created, "we don't have it yet");
         items[_index].state = productState.paid;
         items[_index].buyer = msg.sender;
-        emit step(_index,uint(items[_index].state));
+        emit step(_index,uint(items[_index].state),address(items[index].item));
     }
 
     function deliveredProduct(uint _index) public{
         require(items[_index].state==productState.created, "we don't have it yet");
         require(items[_index].buyer==msg.sender, "you didn't bought this");
         items[index].state = productState.delivered;
-        emit step(_index,uint(items[_index].state));
+        emit step(_index,uint(items[_index].state),address(items[index].item));
 
     }
 }
